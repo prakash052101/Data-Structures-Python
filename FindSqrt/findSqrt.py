5ced7e6b98f203e9f69fc3486a6391f39b802a35
prakash052101 <prakash.051601@gmail.com>
2023-12-05 23:02:02 +0530

    #uploading solution of Find Square-Root of x problem of leetcode - 05/12/2023

---
+++ FindSqrt/findSqrt.py
@@ -0,0 +1,26 @@
+#Find Square-Root Problem
+#time complexity - O(log N), where N is the value of the given non-negative integer x.
+
+def mySqrt(x):
+    if x == 0 or x == 1:
+        return x
+    
+    left, right = 1, x // 2
+    
+    while left <= right:
+        mid = left + (right - left) // 2
+        square = mid * mid
+        
+        if square == x:
+            return mid
+        elif square < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+            
+    return right  # Returning right instead of -1 to get the floor value
+
+# Example usage:
+num = 1024
+result = mySqrt(num)
+print(f"The square root of {num} is {result}")
