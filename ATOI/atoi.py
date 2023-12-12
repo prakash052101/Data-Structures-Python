1eb2db8e4f401b156473036780a6c7025444cc5f
prakash052101 <prakash.051601@gmail.com>
2023-12-12 22:12:36 +0530

    #uploading solution of String to Integer (atoi) problem of leetcode - 12/12/2023

---
+++ ATOI/atoi.py
@@ -0,0 +1,59 @@
+#String to Integer Problem
+
+#Method1 - using lstrip and isDigit
+#time complexity - O(n) where n is the length of the input string s
+def atoiM1(s):
+    s = s.strip()  # Remove leading and trailing whitespace
+    if not s:
+        return 0
+    
+    sign = 1
+    result = 0
+    i = 0
+    
+    if s[0] in ['+', '-']:  # Handle sign
+        if s[0] == '-':
+            sign = -1
+        i = 1
+    
+    while i < len(s) and s[i].isdigit():  # Convert digits to integer
+        result = result * 10 + int(s[i])
+        i += 1
+    
+    return max(-2**31, min(sign * result, 2**31 - 1))  # Limit the result to 32-bit signed integer range
+
+
+
+#Method2 - using loops
+#time complexity - O(n) where n is the length of the input string s
+def atoiM2(s):
+    # Remove leading whitespace
+    while s and s[0] == ' ':
+        s = s[1:]
+
+    if not s:
+        return 0
+    
+    sign = 1
+    result = 0
+    i = 0
+    
+    if s[0] == '-':  # Handle negative sign
+        sign = -1
+        i = 1
+    elif s[0] == '+':  # Handle positive sign
+        i = 1
+    
+    # Convert digits to integer
+    while i < len(s) and '0' <= s[i] <= '9':
+        result = result * 10 + ord(s[i]) - ord('0')
+        i += 1
+    
+    return max(-2**31, min(sign * result, 2**31 - 1))  # Limit the result to 32-bit signed integer range
+
+
+if __name__ == "__main__":
+    s1 = "4193 with words"
+    s2 = "   -42"
+    print("Method 1 Output: ", atoiM1(s1))
+    print("Method 2 Output: ", atoiM2(s2))
